f0e6b41e0274874d9bbc5173ad66c37ab63a5d47
arwasu <suhaibaniarwa@gmail.com>
2019-11-04 17:46:31 +0300

    week 4 homework
    How difficult did you find this (out of 10)? 0 being no problems at all, 10 being impossible; 3
    Was there anything that you struggled with?; using the right if statment
    Is there anything that you'd like some further information on?; how to deal with data and statments in reagrds to this task
    Roughly how long did it take? 3hrs

---
+++ ArwaAlsuhaibani/week4/Homework/main.js
@@ -0,0 +1,26 @@
+var num= 0;
+
+function findFactor (num) {
+if(num % 3==0)
+{
+   console.log ("Pling");
+}
+
+if (num % 5==0)
+{
+    console.log ("Plang");
+}
+
+if (num % 7==0)
+{
+    console.log ("Plong");
+}
+
+else 
+{
+    console.log (num);
+ } 
+}
+ findFactor(28);
+ findFactor(1755);
+ findFactor(34);
